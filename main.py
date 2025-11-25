69b66bf87d22fd76952bad0861db0975c7ccafdb
Ahmed Farid <ahmedferid0@gmail.com>
2025-11-25 11:41:56 +0100

    Args added along with setup for the file

--- main.py
+++ main.py
@@ -1,38 +1,46 @@
 import socket
 import argparse
 from subdomains import sub_domains
+from datetime import datetime
 import os
 def main():
-    parser = argparse.ArgumentParser(description='Subdomain Scanner')
+    parser = argparse.ArgumentParser(description='Subdomain Scanner',
+                                     epilog='python main.py -d example.com'
+                                            'python main.py -d target.org -o results.txt')
     parser.add_argument('-d', '--domain', required=True, help='Domain to scan (e.g. google.com)')
     parser.add_argument('-o', '--output', help='Output file name')
     args = parser.parse_args()
 
+    print(f'Running subdomain scan on {args.domain}..........')
+
     found = []
 
-    for s in sub_domains:
-        domain = f"{s}.{args.domain}"
+    for sub in sub_domains:
+        target = f"{sub}.{args.domain}"
         try:
-            ip = socket.gethostbyname(domain)
+            ip = socket.gethostbyname(target)
             print(f"=============================")
-            print(f"[+] FOUND: {domain} → {ip}")
-            found.append((domain, ip))
+            print(f"[+] FOUND: {target} → {ip}")
+            found.append((target, ip))
             print(f"=============================")
-        except:
+        except socket.gaierror:
+            print("No DNS records")
             pass
-
 
     if found:
         if args.output:
-            with open(args.output, mode="w", encoding="utf-8") as file:
-                file.write(f"Subdomain scan results for {args.domain}\n")
-                file.write("="*50 + "\n\n")
+            with open(args.output, mode="w", encoding="utf-8") as report:
+                report.write(f"Subdomain scan results for {args.domain}\n")
+                date_of_the_scan = datetime.now().strftime("%d/%m/%Y %S:%M:%H")
+                report.write(f"Scan done in {date_of_the_scan}\n")
+                report.write("="*50 + "\n\n")
+                report.write(f"Available subdomains checked {len(found)}\n")
+
                 for domain, ip in found:
-                    file.write(f"{domain} --> {ip} \n")
-                file.write(f"Total findings: {len(found)} \n")
+                    report.write(f"{domain} --> {ip} \n")
             print(f"Results saved in {args.output}")
         else:
-            print("No results saved")
+            print("[*] No output file specified. Use -o to save results.")
     else:
         print("No subdomains found to save")
 
